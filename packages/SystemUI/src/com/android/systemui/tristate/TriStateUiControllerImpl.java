59afe3311716caa846860d5ba882f49f5b82eb8e
Pranav Vashi <neobuddy89@gmail.com>
2023-11-01 13:37:17 +0530

    AlertSlider: Fixup implementation on A14
    Change-Id: I4d69b783c0067b98da94cc46ed12663ecfe63e7e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tristate/TriStateUiControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/tristate/TriStateUiControllerImpl.java
@@ -48,7 +48,6 @@ import android.view.WindowManager.LayoutParams;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.systemui.Dependency;
 import com.android.systemui.res.R;
 import com.android.systemui.tristate.TriStateUiController;
 import com.android.systemui.tristate.TriStateUiController.UserActivityListener;
@@ -79,6 +78,8 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
 
     private Context mContext;
     private final VolumeDialogController mVolumeDialogController;
+    private final ConfigurationController mConfigurationController;
+
     private final Callbacks mVolumeDialogCallback = new Callbacks() {
         @Override
         public void onShowRequested(int reason, boolean keyguardLocked, int lockTaskModeState) { }
@@ -114,8 +115,11 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
         public void onCaptionComponentStateChanged(
                 Boolean isComponentEnabled, Boolean fromTooltip) {}
 
-	@Override
-	public void onVolumeChangedFromKey() {}
+        @Override
+        public void onVolumeChangedFromKey() {}
+
+        @Override
+        public void onCaptionEnabledStateChanged(Boolean isEnabled, Boolean checkBeforeSwitch) {}
 
         @Override
         public void onConfigurationChanged() {
@@ -198,8 +202,13 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
         }
     }
 
-    public TriStateUiControllerImpl(Context context) {
+    public TriStateUiControllerImpl(
+            Context context,
+            VolumeDialogController volumeDialogController,
+            ConfigurationController configurationController) {
         mContext = context;
+        mVolumeDialogController = volumeDialogController;
+        mConfigurationController = configurationController;
         mHandler = new H(this);
         mOrientationListener = new OrientationEventListener(mContext, 3) {
             @Override
@@ -207,7 +216,6 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
                 checkOrientationType();
             }
         };
-        mVolumeDialogController = (VolumeDialogController) Dependency.get(VolumeDialogController.class);
         mIntentAction = mContext.getResources().getString(com.android.internal.R.string.config_alertSliderIntent);
         mIntentActionSupported = mIntentAction != null && !mIntentAction.isEmpty();
 
@@ -233,13 +241,13 @@ public class TriStateUiControllerImpl implements ConfigurationListener, TriState
         mWindowType = windowType;
         mDensity = mContext.getResources().getConfiguration().densityDpi;
         mListener = listener;
-        ((ConfigurationController) Dependency.get(ConfigurationController.class)).addCallback(this);
+        mConfigurationController.addCallback(this);
         mVolumeDialogController.addCallback(mVolumeDialogCallback, mHandler);
         initDialog();
     }
 
     public void destroy() {
-        ((ConfigurationController) Dependency.get(ConfigurationController.class)).removeCallback(this);
+        mConfigurationController.removeCallback(this);
         mVolumeDialogController.removeCallback(mVolumeDialogCallback);
         mContext.unregisterReceiver(mRingerStateReceiver);
     }
