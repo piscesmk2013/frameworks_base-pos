AOSP-Krypton/frameworks_base

[SQUASH]base: Allow choosing a custom vibration pattern [1/3]
commit e9d414c58280ace5df5601bcddb0724f528f5b3b
Author: Ido Ben-Hur <idoybh2@gmail.com>
Date:   Fri Jun 5 22:21:04 2020 +0300

    base: Allow choosing a custom vibration pattern [1/3]

    Change-Id: I0d00f94b185a1032deb3e8413720d47a411cd6f7

commit a29a2a4cdbcc5ceb7acad4d89c448e37ff8eeb56
Author: Ido Ben-Hur <idoybh2@gmail.com>
Date:   Mon Feb 22 05:55:26 2021 +0200

    base: Have a proper validator for custom ringtone vibration pattern

    @jhonboy121: validate for upper limit (1000) as well.

    Change-Id: I9657d76f4b961ac25fc52c9f6533549eda356386

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -108,5 +108,6 @@ public class SystemSettings {
         Settings.System.ARTWORK_MEDIA_BACKGROUND_BLUR_RADIUS,
         Settings.System.ARTWORK_MEDIA_BACKGROUND_ALPHA,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
     };
 }

piscesmk2013/frameworks_base-pos

    [jhonboy121]: validate using a ListValidator
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Change-Id: I3eab8d55adb591d463ef761e4fdd7fe5dd302be0
Signed-off-by: spezi77 <spezi7713@gmx.net>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -109,6 +109,7 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
-        Settings.System.RINGTONE_VIBRATION_PATTERN
+        Settings.System.RINGTONE_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN
     };
 }